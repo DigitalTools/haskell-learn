56370e4ac773a2a0d7b5a54e20475ab67a07fa4c
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-14 17:36:33 -0500

    add Guards

---
+++ 04-syntax/guards.hs
@@ -0,0 +1,27 @@
+-- patterns
+-- conforms to some form
+-- and deconstructing it,
+
+bmiTell :: (RealFloat a) => a -> String  
+bmiTell bmi  
+    | bmi <= 18.5 = "You're underweight, you emo, you!"  
+    | bmi <= 25.0 = "You're supposedly normal. Pffft, I bet you're ugly!"  
+    | bmi <= 30.0 = "You're fat! Lose some weight, fatty!"  
+    | otherwise   = "You're a whale, congratulations!"  
+
+-- guards are indicated by pipes
+
+-- there's no = right after the function name and its parameters
+
+max' a b   
+    | a > b     = a  
+    | otherwise = b 
+
+-- backticks
+
+myCompare :: (Ord a) => a -> a -> Ordering  
+a `myCompare` b  
+    | a > b     = GT  
+    | a == b    = EQ  
+    | otherwise = LT 
+
