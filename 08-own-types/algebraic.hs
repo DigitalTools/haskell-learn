e58facda7040d2c7569f2c0b1152ec8bfeb5f77f
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-23 02:34:56 -0500

    add Intermediate Data Type: 🎯 Point

--- 08-own-types/algebraic.hs
+++ 08-own-types/algebraic.hs
@@ -1,13 +1,15 @@
-data Shape = Circle Float Float Float | Rectangle Float Float Float Float deriving (Show)  
+data Point = Point Float Float deriving (Show)  
+-- same "Point" name for data type and calue constructor, because there just one
+data Shape = Circle Point Float | Rectangle Point Point deriving (Show)  
 
 surface :: Shape -> Float
-surface (Circle _ _ radius) = pi * radius^2
-surface (Rectangle x1 y1 x2 y2) = (abs $ x2 - x1) * (abs $ y2 - y1)
+surface ( Circle _ radius ) = pi * radius^2
+surface ( Rectangle (Point x1 y1) (Point x2 y2) ) = (abs $ x2 - x1) * (abs $ y2 - y1)
 
-sc = surface $ Circle 10 20 10
-sr = surface $ Rectangle 0 0 100 100
+sc = surface $ Circle (Point 10 20) 10
+sr = surface $ Rectangle (Point 0 0) (Point 100 100)
 
 -- Circle 10 20 5  
 -- Rectangle 50 230 60 90 
 
-concentricCircles = map (Circle 10 20) [4,5,6,6]
+concentricCircles = map (Circle (Point 10 20)) [4,5,6,6]
