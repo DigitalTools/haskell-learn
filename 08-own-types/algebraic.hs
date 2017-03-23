5aa832fccb833dddc1fef373f8ab273ef3503b7e
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-23 02:49:51 -0500

    adds auxiliary functions to create axis centered shapes

--- 08-own-types/algebraic.hs
+++ 08-own-types/algebraic.hs
@@ -19,3 +19,11 @@ nudge (Circle (Point x y) r) a b = Circle (Point (x+a) (y+b)) r
 nudge (Rectangle (Point x1 y1) (Point x2 y2)) a b = Rectangle (Point (x1+a) (y1+b)) (Point (x2+a) (y2+b))  
 
 mc = nudge (Circle (Point 34 34) 10) 5 10  
+
+baseCircle :: Float -> Shape  
+baseCircle r = Circle (Point 0 0) r  
+  
+baseRect :: Float -> Float -> Shape  
+baseRect width height = Rectangle (Point 0 0) (Point width height)  
+
+nc = nudge (baseRect 40 100) 60 23  
