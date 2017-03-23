2982c8c29d62f8d2efc09cace3a0ebd937ac578b
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-23 14:13:23 -0500

    refactor Person using Record Syntax

--- 08-own-types/record.hs
+++ 08-own-types/record.hs
@@ -1,24 +1,12 @@
-data Person = Person String String Int Float String String deriving (Show)  
+data Person = Person { firstName :: String  
+                     , lastName :: String  
+                     , age :: Int  
+                     , height :: Float  
+                     , phoneNumber :: String  
+                     , flavor :: String  
+                     } deriving (Show)   
 
 guy = Person "Buddy" "Finklestein" 43 184.2 "526-2928" "Chocolate"  
-
-firstName :: Person -> String  
-firstName (Person firstname _ _ _ _ _) = firstname  
-  
-lastName :: Person -> String  
-lastName (Person _ lastname _ _ _ _) = lastname  
-  
-age :: Person -> Int  
-age (Person _ _ age _ _ _) = age  
-  
-height :: Person -> Float  
-height (Person _ _ _ height _ _) = height  
-  
-phoneNumber :: Person -> String  
-phoneNumber (Person _ _ _ _ number _) = number  
-  
-flavor :: Person -> String  
-flavor (Person _ _ _ _ _ flavor) = flavor
 
 gfn = firstName guy
 gh = height guy
