0361a15b5b91fa2e26f194f0c532c89cfb81ce54
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-24 22:20:10 -0500

    infix List addition

--- 08-own-types/recursive-data-structures.hs
+++ 08-own-types/recursive-data-structures.hs
@@ -11,3 +11,12 @@ data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
 -- let a = 3 :-: 4 :-: 5 :-: Empty 
 -- 100 :-: a
 
+infixr 5  .++  
+(.++) :: List a -> List a -> List a   
+Empty .++ ys = ys  
+(x :-: xs) .++ ys = x :-: (xs .++ ys)  
+
+-- a = 3 :-: 4 :-: 5 :-: Empty 
+-- b = 6 :-: 7 :-: Empty  
+-- a .++ b  
+
