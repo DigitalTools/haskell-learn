f94bda0fce8014717144c5b19f0615d8d54acd20
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-24 22:51:41 -0500

    add Prelude.++ infixer definition, as guide

--- 08-own-types/recursive-data-structures.hs
+++ 08-own-types/recursive-data-structures.hs
@@ -11,7 +11,14 @@ data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)
 -- let a = 3 :-: 4 :-: 5 :-: Empty 
 -- 100 :-: a
 
-infixr 5  .++  
+{-
+ -infixr 5  ++ 
+ -(++) :: [a] -> [a] -> [a]  
+ -[]     ++ ys = ys  
+ -(x:xs) ++ ys = x : (xs ++ ys)  
+ -}
+
+infixr 5 .++  
 (.++) :: List a -> List a -> List a   
 Empty .++ ys = ys  
 (x :-: xs) .++ ys = x :-: (xs .++ ys)  
