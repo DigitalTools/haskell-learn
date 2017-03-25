136f3badb4e0677dad7011bd3ca4aafa7e7398c0
Franco Cedillo <franco.cedillo@gmail.com>
2017-03-24 22:15:44 -0500

    add infix constructor for List

--- 08-own-types/recursive-data-structures.hs
+++ 08-own-types/recursive-data-structures.hs
@@ -1,6 +1,13 @@
-data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)  
+-- data List a = Empty | Cons { listHead :: a, listTail :: List a} deriving (Show, Read, Eq, Ord)  
 
 -- 5 `Cons` Empty
 
 -- 4 `Cons` (5 `Cons` Empty)
+
+infixr 5 :-:  
+data List a = Empty | a :-: (List a) deriving (Show, Read, Eq, Ord)  
+
+-- 3 :-: 4 :-: 5 :-: Empty
+-- let a = 3 :-: 4 :-: 5 :-: Empty 
+-- 100 :-: a
 
